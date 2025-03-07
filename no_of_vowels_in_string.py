095542004bd9bfddf1b6b62ea22f4f01568d99fe
beingbhavya01 <bunnylekshmi2004@gmail.com>
2025-03-07 15:08:50 +0530

    Create no_of_vowels_in_string.py

---
+++ no_of_vowels_in_string.py
@@ -0,0 +1,10 @@
+vowels=['a','e','i','o','u','A','E','I','O','U']
+string=input()
+count=0
+for i in range(len(string)):
+    if(string[i] in vowels):
+        count+=1
+if count==0:
+    print("No vowels were found.")
+else:
+    print("Total number of vowels:",count)
